e9da1d10ff2df2976bc39204d4e38f0654fdaea3
bluezdot <72647326+bluezdot@users.noreply.github.com>
2025-10-23 19:10:03 +0700

    add example get calldata

--- src/getCalldata.ts
+++ src/getCalldata.ts
@@ -1,11 +1,18 @@
-import type { ApiPromise } from '@polkadot/api';
-import type { GenericExtrinsic } from '@polkadot/types';
-import { AnyTuple } from '@polkadot/types/types';
-import { blake2AsHex } from '@polkadot/util-crypto';
+import {ApiPromise, WsProvider} from '@polkadot/api';
+import type {GenericExtrinsic} from '@polkadot/types';
+import {AnyTuple} from '@polkadot/types/types';
+import {blake2AsHex} from '@polkadot/util-crypto';
 
 const MULTISIG_EXTRINSIC_CALL_INDEX = 3;
 const WRAP_EXTRINSIC_CALL_INDEX = 2;
 const DEFAULT_BLOCK_HASH = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+export interface GetCallDataParams {
+  api: ApiPromise;
+  callHash: string;
+  blockHeight: number;
+  extrinsicIndex: number;
+}
 
 function nonNullable<T>(val: T | null | undefined): val is T {
   return val !== null && val !== undefined;
@@ -47,13 +54,6 @@ function findInnerExtrinsicCall(extrinsic: GenericExtrinsic<AnyTuple>) {
   return findAsMulti(extrinsic.method);
 }
 
-export interface GetCallDataParams {
-  api: ApiPromise;
-  callHash: string;
-  blockHeight: number;
-  extrinsicIndex: number;
-}
-
 export async function getCallData({api, callHash, blockHeight, extrinsicIndex }: GetCallDataParams) {
   try {
     const blockHash = await api.rpc.chain.getBlockHash(blockHeight);
@@ -80,11 +80,28 @@ export async function getCallData({api, callHash, blockHeight, extrinsicIndex }:
       return null;
     }
 
-    console.log('callData:', callData);
-
     return callData;
   } catch (e) {
     console.warn('Error getCallData()', e);
     return null;
   }
 }
+
+async function example () {
+  const apiPromise = new ApiPromise({
+    provider: new WsProvider('wss://paseo-rpc.dwellir.com')
+  })
+
+  const api = await apiPromise.isReady;
+
+  return await getCallData({
+    api,
+    callHash: '0x76861afd393324589243ffdcec8e75aeeba34cf1d05b1672bb43798ad070cd9e',
+    blockHeight: 8708277,
+    extrinsicIndex: 2
+  });
+}
+
+// calldata: 0x05030010df8d4e15bfd11d276c650b883ea184c8f122ab3b4ae5a6784b9f4722d4595f17000010632d5ec76b05
+
+example().then((rs) => console.log(rs));
